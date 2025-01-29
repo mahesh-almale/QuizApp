cb6620da724a00853e2fce99d554a2a9409bc64f
mahesh-almale <almalemahesh71@gmail.com>
2025-01-29 22:57:08 +0530

    First Commit

---
+++ src/main/java/com/quiz/controller/QuestionController.java
@@ -0,0 +1,45 @@
+package com.quiz.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quiz.model.Questions;
+import com.quiz.serviceimpl.QuestionServiceImpl;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+	
+	@Autowired
+	QuestionServiceImpl quesServiceImpl;
+	
+	@GetMapping("all-questions")
+	public List<Questions> getAllQuestion() {
+		return quesServiceImpl.getallquestions();
+	}
+	
+	@GetMapping("category/{category}")
+	public List<Questions> getQuestionByCategory(@PathVariable String category){
+		return quesServiceImpl.getQuestionByCategory(category);
+		
+	}
+	
+	@GetMapping("{difficultyLevel}")
+	public List<Questions> getQuestionByDifficultyLevel(@PathVariable String difficultyLevel){
+		return quesServiceImpl.getQuestionByDifficultyLevel(difficultyLevel);
+		
+	}
+	@PostMapping("addquestion")
+	public String addQuestion(@RequestBody Questions question) {
+		return quesServiceImpl.addQuestion(question);
+		
+	}
+
+}
---
+++ src/main/java/com/quiz/service/QuestionService.java
@@ -0,0 +1,12 @@
+package com.quiz.service;
+
+import java.util.List;
+
+import com.quiz.model.Questions;
+
+public interface QuestionService {
+	
+	public List<Questions> getallquestions();
+	
+
+}
