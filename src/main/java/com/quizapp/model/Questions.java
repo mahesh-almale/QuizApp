0484343aec341d016f1f3b3557adb362ca3f6917
mahesh-almale <almalemahesh71@gmail.com>
2025-01-31 22:30:22 +0530

    database updated

--- src/main/java/com/quizapp/model/Questions.java
+++ src/main/java/com/quizapp/model/Questions.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 
 @Entity
+@Table(name="quiz_questions")
 public class Questions {
 	
 	@Id
